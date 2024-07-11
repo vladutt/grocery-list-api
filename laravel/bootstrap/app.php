02fba1e6db438b03a3268922087e9b7654aa89a4
Vladut Teodor <vladutteodor18@gmail.com>
2024-07-11 23:25:23 +0300

    api without prefix

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -10,6 +10,8 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        apiPrefix: ''
+
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(append: [
